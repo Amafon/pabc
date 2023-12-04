9af3d83db10652cab57b958f95f5f499f3a3bf4d
Amafon <afy.amafon@gmail.com>
2023-12-04 08:03:22 +0000

    Navigation

---
+++ script.js
@@ -0,0 +1,21 @@
+const navLinks = document.querySelector(".nav-links");
+const menu = document.querySelector(".menu");
+const close = document.querySelector(".close");
+
+menu.addEventListener("click", function () {
+  navLinks.classList.toggle("open");
+});
+
+close.addEventListener("click", function () {
+  navLinks.classList.toggle("open");
+});
+
+navLinks.addEventListener("click", function (e) {
+  const el = e.target;
+  let id = el.href.split("#")[el.href.split("#").length - 1];
+  const section = document.getElementById(id);
+  section.scrollIntoView({
+    behavior: "smooth",
+  });
+  navLinks.classList.toggle("open");
+});
