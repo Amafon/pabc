422d0e5cc5955b34215206375ffa918079523562
Amafon <afy.amafon@gmail.com>
2023-12-08 20:28:30 +0000

    Site Complet V1

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ close.addEventListener("click", function () {
 });
 
 navLinks.addEventListener("click", function (e) {
+  e.preventDefault();
   const el = e.target;
   let id = el.href.split("#")[el.href.split("#").length - 1];
   const section = document.getElementById(id);
